bc6a2aab8ba46de5df1551ea24a90258f7b41ad7
Stefani Blazhevska <stefaniblazhevska@gmail.com config --global user.name Stefanigit config --global user.email stefaniblazhevska@gmail.com>
2023-05-18 11:00:46 +0200

    my first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello world");
+	sayGoodBye("Thursday");
+    }
+
+    public static void sayGoodBye(String name){
+        System.out.println("Good Bye " +name);
+    }
+}
